65874ff1c61a4bba520e1a71ec8c5e28704b60de
Muhammad Zayed <mohamed-zayed@moltaqa.net>
2026-03-18 14:31:30 +0200

    feat: install laravel telescope

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,8 +1,9 @@
 <?php
 
 return [
-    App\Providers\AppServiceProvider::class,
     App\Providers\ApiResponseServiceProvider::class,
+    App\Providers\AppServiceProvider::class,
     App\Providers\MediaUploadServiceProvider::class,
     App\Providers\RepositoryServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
 ];
