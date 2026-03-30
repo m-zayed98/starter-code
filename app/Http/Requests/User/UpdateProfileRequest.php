8e75cdeffbf1c882904a3decd73299596644e6a8
Muhammad Zayed <mohamed-zayed@moltaqa.net>
2026-03-30 16:46:22 +0200

    feat: make the identity and birth date nullable in update profile

--- app/Http/Requests/User/UpdateProfileRequest.php
+++ app/Http/Requests/User/UpdateProfileRequest.php
@@ -28,8 +28,8 @@ class UpdateProfileRequest extends FormRequest
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', Rule::unique('users', 'email')->ignore($user?->id)],
             'phone' => ['required', 'string', 'max:50', Rule::unique('users', 'phone')->ignore($user?->id)],
-            'birth_date' => ['required', 'nullable', 'date'],
-            'identity_number' => ['required', 'nullable', 'string', 'max:50'],
+            // 'birth_date' => ['required', 'nullable', 'date'],
+            // 'identity_number' => ['required', 'nullable', 'string', 'max:50'],
             'avatar' => ['sometimes', 'nullable', 'image', 'mimes:jpeg,png,jpg,webp', 'max:2048'],
         ];
     }
