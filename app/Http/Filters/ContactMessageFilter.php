c10a9d2d89865c02601280ccb29af5b82efa4563
m-zayed9 <mohamed-zayed@moltaqa.net>
2026-05-21 13:06:18 +0300

    fix: remove dd from message type filter

--- app/Http/Filters/ContactMessageFilter.php
+++ app/Http/Filters/ContactMessageFilter.php
@@ -21,7 +21,7 @@ class ContactMessageFilter extends BaseFilters
     /**
      * Filter by search term.
      *
-     * @param string $value
+     * @param  string  $value
      * @return void
      */
     protected function search($value)
@@ -51,14 +51,13 @@ class ContactMessageFilter extends BaseFilters
 
     protected function message_type($value)
     {
-        dd($value);
         $this->builder->where('message_type', $value);
     }
 
     /**
      * Filter by status.
      *
-     * @param string $value
+     * @param  string  $value
      * @return void
      */
     protected function status($value)
